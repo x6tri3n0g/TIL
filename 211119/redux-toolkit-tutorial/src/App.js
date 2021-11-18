734f0739f58c9c3604a199115bb72dcc3864f167
xtring <hh940630@gmail.com>
2021-11-19 03:42:44 +0900

    modify: App.js -components 구성

--- 211119/redux-toolkit-tutorial/src/App.js
+++ 211119/redux-toolkit-tutorial/src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Info from './components/Info';
+
 function App() {
-	return <div>React App</div>;
+	return (
+		<React.Fragment>
+			<section className='todoapp'>
+				<Header />
+				<Main />
+				<Footer />
+			</section>
+			<Info />
+		</React.Fragment>
+	);
 }
 
 export default App;
