2be33ff80bd8e66d1bae2dcd484cd69515ae8fc0
xtring <hh940630@gmail.com>
2021-11-22 01:48:52 +0900

    feat: createSelector 사용

--- 211119/redux-toolkit-tutorial/src/components/Main/index.js
+++ 211119/redux-toolkit-tutorial/src/components/Main/index.js
@@ -1,10 +1,29 @@
 import { useSelector } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
 import Todo from './Todo';
 
 const todosSelector = (state) => state.todos.items;
+const filterTypeSelector = (state) => state.todos.filterType;
+
+// createSelector는 selector들을 매개변수로 받고, 맨 마지막 매개변수는 이 selector들의 결과를 각각 순서에 맞게 매개변수로 받는 selector를 매개변수로 받습니다.
+// 즉, (items, filterType) => ... 에서 items와 filterType은 todosSelector, filterTypeSelector의 리턴값이 들어옵니다.
+const filteredTodosSelector = createSelector(
+	todosSelector,
+	filterTypeSelector,
+	(items, filterType) => {
+		switch (filterType) {
+			case 'do':
+				return items.filter((todo) => !todo.done);
+			case 'done':
+				return items.filter((todo) => todo.done);
+			default:
+				return items;
+		}
+	},
+);
 
 function Main() {
-	const todos = useSelector(todosSelector);
+	const todos = useSelector(filteredTodosSelector);
 	const Todos = todos.map((todo) => <Todo key={todo.id} {...todo} />);
 
 	return (
