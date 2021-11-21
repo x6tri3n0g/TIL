c664b62661c88fea3f00a0c252c51bd40c6c6006
xtring <hh940630@gmail.com>
2021-11-22 01:26:45 +0900

    feat: add text in Header

--- 211119/redux-toolkit-tutorial/src/components/Header.js
+++ 211119/redux-toolkit-tutorial/src/components/Header.js
@@ -1,11 +1,36 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { add as addTodo } from '../state/todos';
+
 function Header() {
+	const [input, setInput] = useState('');
+
+	const dispatch = useDispatch();
+
+	const handleAddTodo = (e) => {
+		if (!(e.keyCode === 14 || e.key === 'Enter')) {
+			return;
+		}
+
+		const text = input.trim();
+		if (text === '') {
+			return;
+		}
+
+		setInput('');
+		dispatch(addTodo(text)); // dispatch()를 통해서 add()를 리턴. store의 state를 업데이트 한다.
+	};
+
 	return (
-		<header>
+		<header className='header'>
 			<h1>todos</h1>
 			<input
-				className='new-todo'
-				placeholder='What needs to be done?'
 				autoFocus
+				className='new-todo'
+				onInput={(e) => setInput(e.target.value)}
+				onKeyDown={handleAddTodo}
+				placeholder='What needs to be done?'
+				value={input}
 			/>
 		</header>
 	);
