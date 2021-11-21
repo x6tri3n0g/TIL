e1f6a7c1cfa4f10f4658ab981bda464cd4cd5a6d
xtring <hh940630@gmail.com>
2021-11-22 02:35:37 +0900

    feat: todo 수정하기

--- 211119/redux-toolkit-tutorial/src/components/Main/Todo.js
+++ 211119/redux-toolkit-tutorial/src/components/Main/Todo.js
@@ -1,11 +1,58 @@
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { check as checkTodo, remove as removeTodo } from '../../state/todos';
+import {
+	check as checkTodo,
+	remove as removeTodo,
+	edit as editTodo,
+} from '../../state/todos';
+
+function getClassName({ done, isEdit }) {
+	const classNames = [];
+	if (done) {
+		classNames.push('completed');
+	}
+
+	if (isEdit) {
+		classNames.push('editing');
+	}
+
+	return classNames.join(' ');
+}
 
 function Todo({ id, done, text }) {
 	const dispatch = useDispatch();
+	const [value, setValue] = useState(text);
+	const [isEdit, setIsEdit] = useState(false);
+	const inputEl = useRef(null);
+
+	useEffect(() => {
+		if (isEdit) {
+			inputEl.current.focus();
+		}
+	}, [isEdit]);
+
+	const edit = () => {
+		const text = value.trim();
+		if (text === '') {
+			return;
+		}
+
+		setIsEdit(false);
+		dispatch(editTodo({ id, text }));
+	};
+
+	const handleEnter = (e) => {
+		if (!(e.keyCode === 13 || e.key === 'Enter')) {
+			return;
+		}
+
+		edit();
+	};
+
+	const className = getClassName({ done, isEdit });
 
 	return (
-		<li className={done ? 'completed' : ''}>
+		<li className={className}>
 			<div className='view'>
 				<input
 					checked={done}
@@ -15,13 +62,20 @@ function Todo({ id, done, text }) {
 					}
 					type='checkbox'
 				/>
-				<label>{text}</label>
+				<label onDoubleClick={() => setIsEdit(true)}>{text}</label>
 				<button
 					className='destroy'
 					onClick={() => dispatch(removeTodo(id))}
 				/>
 			</div>
-			<input className='edit' value='Create a TodoMVC template' />
+			<input
+				ref={inputEl}
+				className='edit'
+				onInput={(e) => setValue(e.target.value)}
+				onKeyDown={handleEnter}
+				onBlur={() => setIsEdit(false)}
+				value={value}
+			/>
 		</li>
 	);
 }
