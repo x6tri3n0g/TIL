7af264cf4ea37c7a3fd88bed68c5374eabc5fc6d
xtring <hh940630@gmail.com>
2023-02-21 00:22:34 +0900

    complete 010

---
+++ node200/1-20/010/index.js
@@ -0,0 +1,9 @@
+const string10 = '10';
+const string30 = '30';
+
+const number10 = 10;
+const number30 = 30;
+
+console.log('string: %s', string10 + string30);
+console.log('number: %d', number10 + number30);
+console.log('string + number: %s', string10 + number30);
