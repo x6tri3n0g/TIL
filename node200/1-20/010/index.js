c0bafc90a91b4a1af724421d2a13093ec341c44f
xtring <hh940630@gmail.com>
2023-02-21 00:23:43 +0900

    complete 010

--- node200/1-20/010/index.js
+++ node200/1-20/010/index.js
@@ -6,4 +6,4 @@ const number30 = 30;
 
 console.log('string: %s', string10 + string30);
 console.log('number: %d', number10 + number30);
-console.log('string + number: %s', string10 + number30);
+console.log('string + number: %s', string10 + number30);  // number + string은 string으로 평가됨
