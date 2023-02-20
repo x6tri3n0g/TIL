7c882037707746b4c6110bc94f4dc8c022ee7072
xtring <hh940630@gmail.com>
2023-02-21 00:02:49 +0900

    complete 006

---
+++ node200/1-20/006/index.js
@@ -0,0 +1,12 @@
+const greeting1 = 'hello';
+const greeting2 = 'bye';
+const name1 = 'xtring';
+const name2 = 'hwang';
+
+const statement1 = `${greeting1}! my name is ${name1}`;
+const statement2 = `${greeting2}! my name is ${name2}`;
+
+console.log(`${greeting1}! my name is ${name1}`);
+console.log(`${greeting2}! my name is ${name2}`);
+console.log(statement1);
+console.log(statement2);
