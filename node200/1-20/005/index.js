7b17ee0ec7f9201c739be238558cfb665fb70e47
xtring <hh940630@gmail.com>
2023-02-20 23:58:41 +0900

    complete 005

---
+++ node200/1-20/005/index.js
@@ -0,0 +1,6 @@
+console.log('name:%s', 'xtring');
+console.log('age:%d', 30);
+console.log('pi:%d', 3.14);
+console.log('math:%d science: %d', 92, 84);
+console.log('name:%s math:%d science:%d ', 'xtring', 113, 772);
+console.log('name:%s math:%s science:%s ', 'xtring', 113, 772);
