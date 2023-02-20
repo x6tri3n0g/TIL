248ad6a7ec61da21f22edaec56e54657b2c348a2
xtring <hh940630@gmail.com>
2023-02-21 00:43:02 +0900

    complete 014

---
+++ node200/1-20/014/index.js
@@ -0,0 +1,7 @@
+const string1 = 'hello';
+const string2 = 'The method then selects elements from the start argument, and up to (but not including) the end argument.';
+
+console.log('string1:', string1.length);  // 5
+console.log('string2:', string2.length);  // 105
+console.log('string1[0]:', string1[0]); // h
+console.log('string2:', string2[104]);  // .
