5274f022dd5d513d8428b16cacf367ba5788d429
xtring <hh940630@gmail.com>
2023-02-21 00:34:10 +0900

    complete 013

---
+++ node200/1-20/013/index.js
@@ -0,0 +1,10 @@
+// 나머지 구하기 - 홀수(odd), 짝수(even) 판단
+
+console.log('5 % 2 = ', 5 % 2);
+
+const numbers = [5, 6, 7, 8];
+
+console.log(numbers[0], 'is even number:', numbers[0] % 2 === 0); // 홀수
+console.log(numbers[1], 'is even number:', numbers[1] % 2 === 0); // 짝수
+console.log(numbers[2], 'is even number:', numbers[2] % 2 === 0); // 홀수
+console.log(numbers[3], 'is even number:', numbers[3] % 2 === 0); // 짝수
