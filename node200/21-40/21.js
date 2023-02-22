e42ea6ceedbead4558cce65955867a36f2a7c3e5
xtring <hh940630@gmail.com>
2023-02-22 23:46:26 +0900

    complete 021

---
+++ node200/21-40/21.js
@@ -0,0 +1,11 @@
+// typeof로 변수의 타입(형) 알아보기
+const pi = 3.14;
+const myName = 'xtring';
+
+console.log('hello : %s', typeof 'hello');
+console.log('"20" : %s', typeof '20');
+console.log('pi : %s', typeof pi);
+console.log('myName : %s', typeof myName);
+console.log('30 : %s', typeof 30);
+console.log('[] : %s', typeof []);
+console.log('{} : %s', typeof {});
