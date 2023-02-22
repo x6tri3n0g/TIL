c2dcd43ba6c42505b6ae176ed67ffffaf6707106
xtring <hh940630@gmail.com>
2023-02-22 23:54:34 +0900

    complete 23

---
+++ node200/21-40/23.js
@@ -0,0 +1,12 @@
+// 증감 연산자
+let number = 1;
+
+console.log('number: ', number);
+number += 1;
+console.log('after number += 1: ', number);
+number -= 1;
+console.log('after number -= 1: ', number);
+number += 10;
+console.log('after number += 10: ', number);
+number -= 5;
+console.log('after number -= 5: ', number);
