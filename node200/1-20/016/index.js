dd0be1d49f1fbf1992ba2233e3ca8d3413dbd1f4
xtring <hh940630@gmail.com>
2023-02-22 23:28:57 +0900

    complete 016

---
+++ node200/1-20/016/index.js
@@ -0,0 +1,13 @@
+// 배열(array) 선언하기
+
+const numbers = [1, 2, 3];
+const strings = ['hello', 'bye', 'welcome'];
+
+// new Array()를 이용하는 방법은 []를 이용하는 방법과 동일
+const numbers2 = new Array(1 ,2 ,3);
+const strings2 = new Array('hello', 'bye', 'welcome');
+
+console.log('numbers :', numbers);
+console.log('numbers2 :', numbers2);
+console.log('strings :', strings);
+console.log('strings2 :', strings2);
