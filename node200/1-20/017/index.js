f92d4b21d59d2cca7068f6224881f897bd9d8923
xtring <hh940630@gmail.com>
2023-02-22 23:30:57 +0900

    complete 017

---
+++ node200/1-20/017/index.js
@@ -0,0 +1,11 @@
+// 배열에 값 넣기
+const arrNumbers = [];
+arrNumbers.push(1);
+arrNumbers.push(2);
+arrNumbers.push(3);
+
+const arrTexts = [];
+arrTexts.push('hello', 'welcome', 'bye');
+
+console.log(arrNumbers);
+console.log(arrTexts);
