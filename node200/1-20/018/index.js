b733999004ecc7d6151e0edca6fea56299b5a3a2
xtring <hh940630@gmail.com>
2023-02-22 23:36:30 +0900

    complete 018

---
+++ node200/1-20/018/index.js
@@ -0,0 +1,8 @@
+// 배열 출력하기
+const arrCoffee = [];
+console.log(arrCoffee[0]);  // undefined
+console.log(arrCoffee.length);  // 0
+
+arrCoffee.push('아메리카노', '라떼', '카푸치노');
+console.log(arrCoffee.length);  // 3
+console.log(arrCoffee[0]);  // 아메리카노
