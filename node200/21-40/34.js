36e04e5c87c9dce65fda600c4411e2a668d12e2a
xtring <hh940630@gmail.com>
2023-02-23 23:52:46 +0900

    complete 34

---
+++ node200/21-40/34.js
@@ -0,0 +1,11 @@
+// for of
+const userList = [
+  { name: 'xtring', age: 31, score: 85 },
+  { name: 'hyun', age: 31, score: 95 },
+  { name: 'hwang', age: 31, score: 76 },
+];
+
+for (const user of userList) {
+  console.log('user: ', user);
+}
+
