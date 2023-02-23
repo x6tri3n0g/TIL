e346710bf9ef1ae426440a6a8ecfd0ed3b8b1aaa
xtring <hh940630@gmail.com>
2023-02-23 23:17:42 +0900

    complete 27

---
+++ node200/21-40/27.js
@@ -0,0 +1,14 @@
+// 조건문 if
+if (true) {
+  console.log('조건이 true일 때 실행됩니다.');
+}
+
+const number = 100;
+
+if (number >= 100) {
+  console.log('number는 100보다 큽니다.');
+}
+
+if (number > 200) {
+  console.log('number는 200보다 큽니다.');
+}
