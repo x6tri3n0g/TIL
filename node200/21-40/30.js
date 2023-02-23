450182c2c93796b7437926e0a506d6c74c1eaa41
xtring <hh940630@gmail.com>
2023-02-23 23:38:11 +0900

    complete 30

---
+++ node200/21-40/30.js
@@ -0,0 +1,25 @@
+// switch
+const number = 3;
+
+let message = '';
+
+switch(number) {
+  case 1:
+    message = '값이 아닙니다.';
+    break;
+  case 2:
+    message = '값이 아닙니다.';
+    break;
+  case 3:
+    message = '찾는 값입니다.';
+    break;
+  case 4:
+    message = '값이 아닙니다.';
+    break;
+  case 5:
+    message = '값이 아닙니다.';
+    break;
+  default:
+}
+
+console.log(message);
