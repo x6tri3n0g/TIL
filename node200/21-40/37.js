5d7fd2dc4c5e6dc95b62b6a9946306f078a6e312
xtring <hh940630@gmail.com>
2023-03-02 23:39:42 +0900

    complete 37

---
+++ node200/21-40/37.js
@@ -0,0 +1,13 @@
+const today = new Date();
+const date1 = new Date(2023, 9 -1, 2);
+const date2 = new Date(1994, 8, 3);
+const date3 = new Date(2021, 8, 4, 30);
+const date4 = new Date(2021, 8, 4, 30, 20);
+const date5 = new Date(2021, 8, 4, 30, 20, 10);
+
+console.log(today.toLocaleString());
+console.log(date1.toLocaleString());
+console.log(date2.toLocaleString());
+console.log(date3.toLocaleString());
+console.log(date4.toLocaleString());
+console.log(date5.toLocaleString());
