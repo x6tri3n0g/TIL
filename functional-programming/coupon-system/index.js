2f228d410e40c675465effdea53f1e49d855e7b9
xtring <hh940630@gmail.com>
2023-04-30 18:36:56 +0900

    functional-programming/coupon-system 데이터 준비

---
+++ functional-programming/coupon-system/index.js
@@ -0,0 +1,54 @@
+var emailData = [
+    {
+        email: 'john@coldmail.com',
+        rec_count: 2
+    },
+    {
+        email: 'sam@pmail.com',
+        rec_count: 16
+    },
+    {
+        email: 'linda1989@oal.com',
+        rec_count: 1
+    },
+    {
+        email: 'jan1940@ahoy.com',
+        rec_count: 0
+    },
+    {
+        email: 'mrbig@pmail.com',
+        rec_count: 25
+    },
+    {
+        email: 'olo@lol.co',
+        rec_count: 0
+    },
+];
+var couponData = [
+    {
+        coupon: 'MAYDISCOUNT',
+        rank: 'good'
+    },
+    {
+        coupon: '10PERCENT',
+        rank: 'bad'
+    },
+    {
+        coupon: 'PROMOTION45',
+        rank: 'best'
+    },
+    {
+        coupon: 'IHEARTYOU',
+        rank: 'bad'
+    },
+    {
+        coupon: 'GETADEAL',
+        rank: 'best'
+    },
+    {
+        coupon: 'ILIKEDISCOUNTS',
+        rank: 'good'
+    },
+];
+console.log(emailData);
+console.log(couponData);
