45426e2526a2514eca583b5894095e91e3205f3d
xtring <hh940630@gmail.com>
2023-06-03 16:02:14 +0900

    modify/copy-on-right-function-2

--- functional-programming/mailing-list/index.ts
+++ functional-programming/mailing-list/index.ts
@@ -3,14 +3,14 @@ const mailingList: string[] = [];
 // Action
 const submitForm = (_: string[]) => undefined;
 
-const addListLast = (email: string) => {
-  return [...mailingList, email];
+const addListLast = (mailList: string[], email: string) => {
+  return [...mailList, email];
 }
 
 const submitFormHandler = (event: HTMLFormElement) => {
   const form = event.currentTarget;
   const email = form.elements['email'].value;
-  const newEmailList = addListLast(email);
+  const newEmailList = addListLast(mailingList, email);
   submitForm(newEmailList);
 }
 
