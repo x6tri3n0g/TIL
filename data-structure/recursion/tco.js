ba292b8833827a07856777adb2b9fc2ebcf3e774
xtring <hh940630@gmail.com>
2025-10-14 20:30:08 +0900

    add: tco.js

---
+++ data-structure/recursion/tco.js
@@ -0,0 +1,18 @@
+/** TCO
+ * @description Tail Call Optimization
+ * @param num
+ * @param acc
+ * @returns
+ *
+ * sudo code
+ * 1. 만약 num이 0이면 acc를 반환한다.
+ * 2. 그렇지 않으면 num과 acc를 곱하고 그 결과를 반환한다.
+ */
+function fatorial(num, acc) {
+    if (acc === void 0) { acc = 1; }
+    if (num === 0)
+        return acc;
+    return fatorial(num - 1, acc * num);
+}
+var number = 5;
+console.log(number + "! = " + fatorial(number));
